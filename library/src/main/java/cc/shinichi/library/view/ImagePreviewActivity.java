16e8f2b1f0cfa0d8fe5f8a890fdbc9478e447fdc
勾勾 <gougou@16fan.com>
2018-09-18 13:45:05 +0800

    优化21以上状态栏完全透明；增加是否显示关闭按钮功能；TODO：下拉图片关闭功能

--- library/src/main/java/cc/shinichi/library/view/ImagePreviewActivity.java
+++ library/src/main/java/cc/shinichi/library/view/ImagePreviewActivity.java
@@ -5,7 +5,9 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
@@ -17,6 +19,8 @@ import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -49,6 +53,7 @@ public class ImagePreviewActivity extends AppCompatActivity
   private int currentItem;// 当前显示的图片索引
   private String downloadFolderName = "";// 保存的文件夹名
   private boolean isShowDownButton;
+  private boolean isShowCloseButton;
   private boolean isShowOriginButton;
 
   private ImagePreviewAdapter imagePreviewAdapter;
@@ -57,6 +62,7 @@ public class ImagePreviewActivity extends AppCompatActivity
   private FrameLayout fm_image;
   private TextView tv_show_origin;
   private ImageView img_download;
+  private ImageView imgCloseButton;
 
   private String currentItemOriginPathUrl = "";// 当前显示的原图链接
   private HandlerUtils.HandlerHolder handlerHolder;
@@ -74,13 +80,29 @@ public class ImagePreviewActivity extends AppCompatActivity
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_preview);
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+      Window window = getWindow();
+      window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+      window.getDecorView()
+          .setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+      window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+      window.setStatusBarColor(Color.TRANSPARENT);
+    } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+      getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+    }
+
     context = this;
     handlerHolder = new HandlerUtils.HandlerHolder(this);
 
     imageInfoList = ImagePreview.getInstance().getImageInfoList();
+    if (null == imageInfoList || imageInfoList.size() == 0) {
+      onBackPressed();
+    }
     currentItem = ImagePreview.getInstance().getIndex();
     downloadFolderName = ImagePreview.getInstance().getFolderName();
     isShowDownButton = ImagePreview.getInstance().isShowDownButton();
+    isShowCloseButton = ImagePreview.getInstance().isShowCloseButton();
 
     currentItemOriginPathUrl = imageInfoList.get(currentItem).getOriginUrl();
 
@@ -95,6 +117,10 @@ public class ImagePreviewActivity extends AppCompatActivity
     fm_image = findViewById(R.id.fm_image);
     tv_show_origin = findViewById(R.id.tv_show_origin);
     img_download = findViewById(R.id.img_download);
+    imgCloseButton = findViewById(R.id.imgCloseButton);
+
+    // 关闭页面按钮
+    imgCloseButton.setOnClickListener(this);
     // 查看与原图按钮
     tv_show_origin.setOnClickListener(this);
     // 下载图片按钮
@@ -112,10 +138,15 @@ public class ImagePreviewActivity extends AppCompatActivity
       img_download.setVisibility(View.GONE);
     }
 
+    if (isShowCloseButton) {
+      imgCloseButton.setVisibility(View.VISIBLE);
+    } else {
+      imgCloseButton.setVisibility(View.GONE);
+    }
+
     // 更新进度指示器
     tv_indicator.setText(
-        String.format(getString(R.string.indicator), currentItem + 1 + " ",
-            " " + imageInfoList.size()));
+        String.format(getString(R.string.indicator), currentItem + 1 + "", "" + imageInfoList.size()));
 
     imagePreviewAdapter = new ImagePreviewAdapter(this, imageInfoList);
     viewPager.setAdapter(imagePreviewAdapter);
@@ -132,8 +163,7 @@ public class ImagePreviewActivity extends AppCompatActivity
         }
         // 更新进度指示器
         tv_indicator.setText(
-            String.format(getString(R.string.indicator), currentItem + 1 + " ",
-                " " + imageInfoList.size()));
+            String.format(getString(R.string.indicator), currentItem + 1 + "", "" + imageInfoList.size()));
       }
     });
   }
@@ -257,6 +287,8 @@ public class ImagePreviewActivity extends AppCompatActivity
       }
     } else if (i == R.id.tv_show_origin) {
       handlerHolder.sendEmptyMessage(0);
+    } else if (i == R.id.imgCloseButton) {
+      onBackPressed();
     }
   }
 
